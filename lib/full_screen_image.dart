9d189993bc135d3bd3a93227d36426fbe0411da0
Furkankyl <furkan.kayali@bil.omu.edu.tr>
2020-02-04 11:56:11 +0300

    initial

---
+++ lib/full_screen_image.dart
@@ -0,0 +1,180 @@
+library full_screen_image;
+
+import 'package:flutter/material.dart';
+
+class FullScreenWidget extends StatelessWidget {
+
+  FullScreenWidget({@required this.child, this.backgroundColor = Colors.black,
+    this.backgroundIsTransparent= true, this.disposeLevel});
+
+
+  final Widget child;
+  final Color backgroundColor;
+  final bool backgroundIsTransparent;
+  final DisposeLevel disposeLevel;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, PageRouteBuilder(
+            opaque: false,
+            barrierColor: Colors.white.withOpacity(0),
+            pageBuilder: (BuildContext context,_,__) {
+              return FullScreenPage(
+                child: child,
+                backgroundColor: backgroundColor,
+                backgroundIsTransparent: backgroundIsTransparent,
+                disposeLevel: disposeLevel,
+              );
+            }
+        ));
+      },
+      child: child,
+    );
+  }
+
+
+}
+
+
+
+enum DisposeLevel {High,Medium,Low}
+
+class FullScreenPage extends StatefulWidget {
+
+  FullScreenPage({@required this.child, this.backgroundColor = Colors.black, this.backgroundIsTransparent = true,
+    this.disposeLevel = DisposeLevel.Medium});
+
+  final Widget child;
+  final Color backgroundColor;
+  final bool backgroundIsTransparent;
+  final DisposeLevel disposeLevel;
+
+
+
+  @override
+  _FullScreenPageState createState() => _FullScreenPageState();
+
+}
+
+class _FullScreenPageState extends State<FullScreenPage> {
+
+
+  double initialPositionY = 0;
+
+  double currentPositionY = 0;
+
+  double positionYDelta = 0;
+
+  double opacity = 1;
+
+  double disposeLimit = 150;
+
+  @override
+  void initState() {
+
+    super.initState();
+    setDisposeLevel();
+  }
+
+  setDisposeLevel(){
+    setState(() {
+      if(widget.disposeLevel == DisposeLevel.High)
+        disposeLimit = 300;
+      else if(widget.disposeLevel == DisposeLevel.Medium)
+        disposeLimit =200;
+      else
+        disposeLimit = 100;
+
+
+    });
+  }
+
+
+  void _startVerticalDrag(details) {
+    setState(() {
+
+      initialPositionY = details.globalPosition.dy;
+    });
+  }
+
+  void _whileVerticalDrag(details) {
+    setState(() {
+      currentPositionY = details.globalPosition.dy;
+      positionYDelta = currentPositionY - initialPositionY;
+      setOpacity();
+    });
+
+  }
+
+
+  setOpacity(){
+    double tmp = positionYDelta <0? 1- ((positionYDelta /1000)*-1): 1- (positionYDelta / 1000);
+    print(tmp);
+
+    if(tmp>1)
+      opacity = 1;
+    else if(tmp <0)
+      opacity = 0;
+    else
+      opacity = tmp;
+
+    if(positionYDelta > disposeLimit || positionYDelta < -disposeLimit){
+      opacity = 0.5;
+    }
+  }
+
+  _endVerticalDrag(DragEndDetails details) {
+    if (positionYDelta > disposeLimit || positionYDelta < -disposeLimit) {
+      Navigator.of(context).pop();
+    } else {
+
+      setState(() {
+        opacity = 1;
+        positionYDelta = 0;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: widget.backgroundIsTransparent?Colors.transparent:widget.backgroundColor,
+
+      body: GestureDetector(
+        onVerticalDragStart: (details) => _startVerticalDrag(details),
+        onVerticalDragUpdate: (details) => _whileVerticalDrag(details),
+        onVerticalDragEnd: (details) => _endVerticalDrag(details),
+        child: Container(
+          color: widget.backgroundColor.withOpacity(opacity ),
+          constraints: BoxConstraints.expand(
+            height: MediaQuery
+                .of(context)
+                .size
+                .height,
+          ),
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: 0 + positionYDelta,
+                bottom: 0 - positionYDelta,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: MediaQuery.of(context).size.width,
+                  width: MediaQuery.of(context).size.width,
+                  child: Opacity(
+                    opacity: opacity==0?0.5:opacity,
+                    child: widget.child,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
